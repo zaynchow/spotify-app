c65d34c85ad8746c4ef3c04c25cc89f0bc46d66d
zaynchow <83538743+zaynchow@users.noreply.github.com>
2022-01-25 18:06:42 -0800

    integrated concurrently to manage two package.json files efficiently

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,15 @@ function App() {
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const access_token = urlParams.get("access_token");
-    const refresh_token = urlParams.get("refresh_token");
+    const accessToken = urlParams.get("access_token");
+    const refreshToken = urlParams.get("refresh_token");
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    }
   }, []);
 
   return (
