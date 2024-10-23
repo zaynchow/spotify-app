2eb9d91f61cb4803272287d818e49a289c85a8cf
Zayn Chowdhury <zaynchowdhury763@gmail.com>
2024-10-23 04:22:00 -0700

    updated dependencies and added test login info

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,6 +6,10 @@ const StyledLoginContainer = styled.main`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  .test-login-container {
+    margin-top: 60px;
+    text-align: center;
+  }
 `;
 
 const StyledLoginButton = styled.a`
@@ -29,6 +33,10 @@ const Login = () => (
     <StyledLoginButton href={`${process.env.REACT_APP_PROD_BASE_URL}/login`}>
       Log in to Spotify
     </StyledLoginButton>
+    <div className="test-login-container">
+      <h5>Test Email Address: spotify.analytics.test@gmail.com</h5>
+      <h5>Test Password: Spotify123@</h5>
+    </div>
   </StyledLoginContainer>
 );
 
